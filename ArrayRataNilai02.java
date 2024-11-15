e8a41765c2294c1a862f79100af4821e3e70d08b
azaria amanda <azaryaamanda@gmail.com>
2024-11-15 21:29:55 +0700

    Modifikasi Percobaan 3

--- ArrayRataNilai02.java
+++ ArrayRataNilai02.java
@@ -2,10 +2,14 @@ import java.util.Scanner;
 public class ArrayRataNilai02 {
     public static void main(String[] args) {
         Scanner sc2 = new Scanner(System.in);
+        
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jmlMhs = sc2.nextInt();
 
-        int[] nilaiMhs = new int [10];
+        int[] nilaiMhs = new int[jmlMhs]; 
         double total = 0;
-        double rata2;
+        double rata2Lulus, rata2TdkLulus, totalMhsLulus=0, totalMhsTdkLulus=0;
+        int lulus=0, tidakLulus=0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + " : ");
@@ -14,7 +18,18 @@ public class ArrayRataNilai02 {
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
         }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = " + rata2);
+        for (int i = 0; i < nilaiMhs.length; i++){
+            if (nilaiMhs[i] > 70){
+                totalMhsLulus += nilaiMhs[i];
+                lulus++;
+            } else {
+                totalMhsTdkLulus += nilaiMhs[i];
+                tidakLulus++;
+            }
+        }
+        rata2Lulus = totalMhsLulus/lulus;
+        rata2TdkLulus = totalMhsTdkLulus/tidakLulus;
+        System.out.println("Rata-rata nilai lulus = " + rata2Lulus);
+        System.out.println("Rata-rata nilai tidak lulus = " + rata2TdkLulus);
     }
 }
