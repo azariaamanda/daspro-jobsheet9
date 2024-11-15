118438061b8696a0cabd7de805978fc94f4eade1
azaria amanda <azaryaamanda@gmail.com>
2024-11-15 12:13:26 +0700

    Percobaan 2

---
+++ ArrayNilai02.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ArrayNilai02 {
+    public static void main(String[] args) {
+        Scanner sc2 = new Scanner(System.in);
+
+        int[] nilaiAkhir = new int[10];
+
+        for (int i=0; i < 10; i++){
+            System.out.print("Masukkan nilai akhir ke-" + i + " : ");
+            nilaiAkhir[i] = sc2.nextInt();
+        }
+        for (int i=0; i < 10; i++) {
+            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
+        }
+       
+    }
+    
+}
